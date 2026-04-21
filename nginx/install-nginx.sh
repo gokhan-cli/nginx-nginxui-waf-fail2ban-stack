1251905a736794f55bbd7a9daccb60b2fd6dbd44
gokhan-cli <gokhansezen@gmail.com>
2026-04-21 10:36:44 +0300

    Update install-nginx.sh

--- nginx/install-nginx.sh
+++ nginx/install-nginx.sh
@@ -3,12 +3,7 @@
 # İşletim sisteminize uygun Nginx sürümünü kurun ve kurulan sürümü kontrol edin:
 
 sudo apt update
-sudo apt install nginx=1.29.4-1~noble curl wget unzip -y
-
-# WAF uyumluluğunun bozulmaması için Nginx sürümünü sabitliyoruz (hold):
-sudo apt-mark hold nginx
-
-nginx -v # Çıkan sürüm: nginx version: nginx/1.29.4
+sudo apt install nginx curl wget unzip -y
 
 sudo systemctl enable nginx
 sudo systemctl start nginx
